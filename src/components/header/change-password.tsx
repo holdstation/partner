0046c9f9005d8b61ed7c31ee43e212cf721cf707
huyvx <huy46.it@gmail.com>
2025-10-30 16:56:25 +0700

    change password

--- src/components/header/change-password.tsx
+++ src/components/header/change-password.tsx
@@ -1,4 +1,5 @@
-import { Button, Flex, Modal } from "antd";
+import { LoadingOutlined } from "@ant-design/icons";
+import { Button, Flex, Form, Input, Modal, Result, Spin } from "antd";
 import { useCallback, useEffect, useState } from "react";
 
 function saveFlow(flow: any) {
@@ -41,10 +42,41 @@ async function getChangePasswordFlow() {
     );
 
     flow = await res.json();
-    saveFlow(flow);
+    if (res.ok) {
+      saveFlow(flow);
+    }
   }
 
   return flow;
+}
+
+export function extractMethodAndCsrf(flow: any) {
+  if (!flow) {
+    return { method: "", csrf_token: "", action: "", email: "" };
+  }
+  let csrf_token: string = "";
+  let method: string | undefined;
+  let email: string = "";
+
+  for (const node of flow.ui.nodes) {
+    const { name, value, type } = node.attributes;
+
+    // lấy csrf_token
+    if (name === "csrf_token") {
+      csrf_token = value || "";
+    }
+
+    if (name === "traits.email" && type == "email") {
+      email = value || "";
+    }
+
+    // lấy method (submit button có type=submit)
+    if (name === "method" && type === "submit") {
+      method = value;
+    }
+  }
+
+  return { method, csrf_token: csrf_token, action: flow.ui.action, email };
 }
 
 export function ChangePassword() {
@@ -86,31 +118,122 @@ export function ChangePassword() {
 }
 
 function FormChangePassword(props: { onClose: () => void }) {
- 
-  const [flow, setFlow] = useState(false);
+  const [flow, setFlow] = useState();
+  const [loading, setLoading] = useState(false);
   console.log(flow);
 
+  const { csrf_token, method, action, email } = extractMethodAndCsrf(flow);
+
   const handleGetFlow = useCallback(async () => {
-    const flow = await getChangePasswordFlow();
-    setFlow(flow);
+    setLoading(true);
+    try {
+      const flow = await getChangePasswordFlow();
+      setFlow(flow);
+    } finally {
+      setLoading(false);
+    }
   }, []);
 
   useEffect(() => {
     handleGetFlow();
   }, [handleGetFlow]);
 
-  return (
-    <div>
-      <div>111</div>
-      <Flex className="mt-4" justify="end">
-        <Button
-          onClick={() => {
-            props.onClose();
-          }}
-        >
-          Close
-        </Button>
+  if (loading) {
+    return (
+      <Flex align="center" gap="middle" justify="center">
+        <Spin indicator={<LoadingOutlined spin />} />
       </Flex>
+    );
+  }
+
+  if (!loading && !flow) {
+    return (
+      <Result
+        status="403"
+        title="403"
+        subTitle="Sorry, you are not authorized to access"
+        extra={
+          <Button
+            type="primary"
+            onClick={() => {
+              props.onClose();
+            }}
+          >
+            Close
+          </Button>
+        }
+      />
+    );
+  }
+
+  return (
+    <div className="mt-6">
+      <Form layout="vertical" action={action} method={"POST"}>
+        <Form.Item name={"csrf_token"} hidden={true}>
+          <Input value={csrf_token} />
+        </Form.Item>
+        <Form.Item label="email" name="traits.email" hidden={true}>
+          <Input name="traits.email" value={email} />
+        </Form.Item>
+        <Form.Item label="method" name="method" hidden={true}>
+          <Input name="method" value={method} />
+        </Form.Item>
+
+        <Form.Item
+          label="Password"
+          name="password"
+          required
+          rules={[
+            {
+              required: true,
+              message: "Password is required",
+            },
+            {
+              min: 8,
+              message: "Password must be at least 8 characters long",
+            },
+          ]}
+        >
+          <Input name="password" />
+        </Form.Item>
+        <Form.Item
+          label="Confirm Password"
+          name="password2"
+          required
+          dependencies={["password"]}
+          rules={[
+            {
+              required: true,
+            },
+            ({ getFieldValue }) => ({
+              validator(_, value) {
+                if (!value || getFieldValue("password") === value) {
+                  return Promise.resolve();
+                }
+                return Promise.reject(
+                  new Error("The new password that you entered do not match!")
+                );
+              },
+            }),
+          ]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item>
+          <Flex className="mt-4" justify="end" gap={8}>
+            <Button
+              onClick={() => {
+                props.onClose();
+              }}
+            >
+              Close
+            </Button>
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Flex>
+        </Form.Item>
+      </Form>
     </div>
   );
 }
