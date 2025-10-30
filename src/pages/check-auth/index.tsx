1ae58f81635e0b77a3d1563e96d268cf4071b68d
huyvx <huy46.it@gmail.com>
2025-10-30 11:02:01 +0700

    partner

--- src/pages/check-auth/index.tsx
+++ src/pages/check-auth/index.tsx
@@ -1,11 +1,25 @@
-import { Outlet } from "react-router";
+import { useLogin } from "@refinedev/core";
+import { useEffect, useMemo } from "react";
+import { Outlet, useSearchParams } from "react-router";
 
 export function AuthCallback() {
-  // const { mutate } = useLogin();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const { mutateAsync } = useLogin();
 
-  // useEffect(() => {
-  //   mutate({ type: "check" });
-  // }, [mutate]);
+  const check = useMemo(() => {
+    return searchParams.get("to")?.startsWith("/login?flow=");
+  }, [searchParams]);
+
+  useEffect(() => {
+    mutateAsync({ type: check ? "" : "check" })
+      .then()
+      .catch(() => {
+        setSearchParams((params) => {
+          params.delete("to");
+          return params;
+        });
+      });
+  }, [check, mutateAsync, setSearchParams]);
 
   return <Outlet />;
 }
