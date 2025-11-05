adb889c2acfce24ac082684fc3fe9d9a4b18660b
huyvx <huy46.it@gmail.com>
2025-11-05 14:32:16 +0700

    fix theme

--- src/components/theme/index.tsx
+++ src/components/theme/index.tsx
@@ -1,18 +1,24 @@
 import { useTheme } from "@/stores/theme";
-import { useShallow } from 'zustand/react/shallow'
+import { useShallow } from "zustand/react/shallow";
 
 import { ConfigProvider, theme as ThemeAntd } from "antd";
+import { useEffect } from "react";
 
 export const ConfigTheme = ({ children }: React.PropsWithChildren) => {
-  const [theme] = useTheme(useShallow(state =>[state.theme, state.setTheme]));
+  const [theme] = useTheme(useShallow((state) => [state.theme, state.setTheme]));
+
+  useEffect(() => {
+    if (theme === "dark") {
+      document.body.style.backgroundColor = "#FFF";
+    } else {
+      document.body.style.backgroundColor = "#000";
+    }
+  }, [theme]);
 
   return (
     <ConfigProvider
       theme={{
-        algorithm:
-          theme === "dark"
-            ? ThemeAntd.defaultAlgorithm
-            : ThemeAntd.darkAlgorithm,
+        algorithm: theme === "dark" ? ThemeAntd.defaultAlgorithm : ThemeAntd.darkAlgorithm,
       }}
     >
       {children}
